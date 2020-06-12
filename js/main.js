9b833ff327378c4f642d53b89bdfc7a8844e56a6
Igor Jean Luc Ndiramiye <nigorjeanluc@gmail.com>
2020-06-12 10:58:27 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("dropdown-content").style.display.block;
+}
+
+function func(){
+  alert('Yes');
+}
+
+// Close the dropdown if the user clicks outside of it
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', myFunction);
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
